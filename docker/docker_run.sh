cf57165d816ebb8247f42b20326a6e709717aa20
arjuncr <arjuncr00@gmail.com>
2019-07-22 00:03:09 +0530

    adding startup script

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -4,5 +4,5 @@ if [ -z "$1" ]
 then
 echo "Enter container name ex: docker_run.sh container-name"
 else
-docker run -dit --name $1 -p 8080:80  apache-docker
+docker run -it --name $1 -p 8080:80  apache-docker
 fi
